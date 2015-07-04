ffe6938b62dea18e1dfe23f579eab369d80da71a
Vladimir Zarypov <krre31@gmail.com>
2015-07-04 10:52:39 +0300

    added command line arguments in application package

--- core/application.go
+++ core/application.go
@@ -3,6 +3,7 @@ package core
 import (
 	"github.com/go-gl/glfw/v3.1/glfw"
 	"github.com/krre/angie3d"
+	"os"
 )
 
 type Application struct {
@@ -10,20 +11,25 @@ type Application struct {
 }
 
 func NewApplication() *Application {
-	application := Application{}
+	app := Application{}
+	app.arguments = os.Args
 
 	err := glfw.Init()
 	if err != nil {
 		panic(err)
 	}
 
-	return &application
+	return &app
 }
 
-func (application *Application) Run() {
+func (app *Application) Run() {
 	defer glfw.Terminate()
 
 	for !angie3d.IsExit {
 		glfw.WaitEvents()
 	}
+}
+
+func (app *Application) GetArguments() []string {
+	return app.arguments
 }
